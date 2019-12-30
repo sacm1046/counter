a66d7b5a9453822c592a267b46b335c539f39d84
Sebastian Contreras <sacm1046@gmail.com>
2019-12-30 10:06:25 -0300

    counter

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// Add css files
+import 'bootstrap/dist/css/bootstrap.min.css';
+//import 'font-awesome/css/font-awesome.min.css'; // 4.x
+import '@fortawesome/fontawesome-free/css/all.min.css'; // 5.x
+
+import 'jquery';
+import 'popper.js';
+import 'bootstrap';
