9c39595d8e8cec716228b52041f100817d36cfd3
Sebastian Contreras <sacm1046@gmail.com>
2019-12-30 10:49:48 -0300

    newCounter

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-// Add css files
+import React from 'react';
+import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-//import 'font-awesome/css/font-awesome.min.css'; // 4.x
-import '@fortawesome/fontawesome-free/css/all.min.css'; // 5.x
+
+import App from './App'
 
 import 'jquery';
 import 'popper.js';
 import 'bootstrap';
+
+ReactDOM.render(
+    <App />,
+    document.querySelector("#root")
+)
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
 const path = require('path');
 module.exports = {
     context: __dirname,
@@ -15,34 +14,25 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: ['babel-loader']
-            }, // for js or jsx files
+                test: /\.js$/,
+                use: 'babel-loader',
+            },
             {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader'],
-            }, // for css files
+            },
             {
                 test: /\.(png|j?g|svg|gif)?$/,
                 use: 'file-loader'
-            }, // for images files
-            { 
-                test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)/, 
-                use: ['file-loader'] 
-            }  // for fonts
-        ],
+            }
+        ]
     },
     plugins: [
         new HtmlWebPackPlugin({
             template: path.resolve(__dirname, 'public/index.html'),
             filename: './index.html'
-        }),
-        new webpack.ProvidePlugin({
-            '$': "jquery",
-            'jQuery': "jquery",
-            'Popper': 'popper.js'
-        }),
-        new webpack.HotModuleReplacementPlugin()
+        })
     ]
 };
+
+
