310dcf851895f76bf38b241f7f3ab1afdc1cd8ee
Sebastian Contreras <sacm1046@gmail.com>
2019-12-30 12:07:11 -0300

    counterNew

--- src/components/cards.js
+++ src/components/cards.js
@@ -1,21 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 
-// Add css files
-import 'bootstrap/dist/css/bootstrap.min.css'; // CSS from Bootstrap
-import '@fortawesome/fontawesome-free/css/all.css';
-import './index.css'; // My own CSS
-
-// Add js files
-import 'jquery';
-import 'popper.js';
-import 'bootstrap';
-
-class Home extends React.Component {
-    render() {
-        return (
-            <div className="card">
+function Card(props) { 
+    return (
+        <div className="card">
                 <img className="card-img-top" src="http://via.placeholder.com/350x150" alt="Card image cap" />
                 <div className="card-body">
                     <h5 className="card-title">Card title</h5>
@@ -23,13 +11,7 @@ class Home extends React.Component {
                     <a href="#" className="btn btn-primary">Go somewhere</a>
                 </div>
             </div>
-        )
-    }
-}
-ReactDOM.render(<Home name="Jonathan M." />, document.querySelector("#root"));
+    );
+ }
 
-
-
-
-
-
+ export default Card;
